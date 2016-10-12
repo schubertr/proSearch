c37167d5decc028a5a554574bec8ec0951db98f5
s-chube <schubertr@etown.edu>
2016-10-12 16:50:13 -0400

    simple homepage & rerouted xammp to use virtual host

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+
+<title>ProSearch</title>
+
+<style>
+
+*{font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif}
+
+</style>
+
+<body background="bkgrd.png">
+
+<h1 style="text-align:center; margin-top: 3%">ProSearch</h1>
+
+<br>
+
+<div style="text-align:center;">Welcome to <strong>Pro Search</strong>, an automated tool to help you get the Google results you're looking for. </div>
+
+</body>
+
+</html>
